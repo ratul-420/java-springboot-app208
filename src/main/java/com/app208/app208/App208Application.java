df9e6833bb4cb5f234d0aca4fd2512d125c265cd
FAHIM MUNTASHIR RATUL <H1566N@hallgato.nje.hu>
2025-11-29 14:16:51 +0100

    Initial Spring Boot project

---
+++ src/main/java/com/app208/app208/App208Application.java
@@ -0,0 +1,13 @@
+package com.app208.app208;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App208Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App208Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/app208/app208/App208ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app208.app208;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App208ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
